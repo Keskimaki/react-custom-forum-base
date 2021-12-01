54600e96abdf03643f2da3c65684af18e769904d
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-12-01 19:00:20 +0200

    Add right bar

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ import Header from './components/Header'
 import Boards from './components/boards'
 import Login from './components/Login'
 import Footer from './components/Footer'
-
+import RightBar from './components/RightBar';
 
 function App() {
   return (
     <div style={styles.app}>
       <Header />
+      <RightBar />
       <Routes>
         <Route path="/boards" element={<Boards />} />
         {/*<Route path="/boards/:boardName" element={<Placeholder />} />*/}
--- src/styles.ts
+++ src/styles.ts
@@ -19,11 +19,28 @@ const headerTab: CSS.Properties = {
   fontWeight: 'bold'
 }
 
+const rightBar: CSS.Properties = {
+  height: '90vh',
+  width: '25vw',
+  //maxWidth: '600px',
+  backgroundColor: '#E1E4E8',
+  position: 'absolute',
+  padding: '10px',
+  right: 0,
+}
+
+const rightBarItem: CSS.Properties = {
+  backgroundColor: '#FFF',
+  padding: '10px',
+  border: '2px solid #586069',
+  marginBottom: '10px'
+}
+
 const board: CSS.Properties = {
   backgroundColor: '#FFF',
   padding: '5px',
-  marginBottom: '10px',
-  fontSize: '20px'
+  margin: '10px',
+  fontSize: '20px',
 }
 
 const link: CSS.Properties = {
@@ -32,17 +49,15 @@ const link: CSS.Properties = {
 }
 
 const login: CSS.Properties = {
-  height: '85vh',
-  width: '600px',
   backgroundColor: '#FFF',
-  paddingTop: '50px',
-  textAlign: 'center',
-  margin: 'auto'
+  padding: '150px 22.5vw',
+  margin: '10px',
+  
 }
 
 const textInput: CSS.Properties = {
-  height: '50px',
-  width: '300px',
+  height: '3vw',
+  width: '25vw',
   justifyContent: 'center',
   alignItems: 'center',
   borderRadius: '5px',
@@ -53,8 +68,8 @@ const textInput: CSS.Properties = {
 const button: CSS.Properties = {
   backgroundColor: '#0366d6',
   color: '#FFF',
-  height: '50px',
-  width: '300px',
+  height: '4vw',
+  width: '25vw',
   justifyContent: 'center',
   alignItems: 'center',
   borderRadius: '5px',
@@ -78,6 +93,8 @@ const styles = {
   app,
   header,
   headerTab,
+  rightBar,
+  rightBarItem,
   board,
   link,
   login,
