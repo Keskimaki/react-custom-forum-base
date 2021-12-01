3b73131dff564302809f0632d9a8f4a18f993319
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-12-01 11:37:01 +0200

    Create header placeholder

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from '../styles'
+
+const Header = () => {
+  return (
+    <div style={styles.header}>
+      <HeaderTab text="Placeholder" />
+      <HeaderTab text="content" />
+      <HeaderTab text="for" />
+      <HeaderTab text="the" />
+      <HeaderTab text="time" />
+      <HeaderTab text="being" />
+    </div>
+  )
+}
+
+const HeaderTab = ({ text }: { text: string }) => {
+  return (
+    <div style={styles.headerTab}>
+      <strong>{text}</strong>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/styles.ts
@@ -0,0 +1,23 @@
+import CSS from 'csstype'
+
+const header: CSS.Properties = {
+  backgroundColor: '#24292e',
+  paddingRight: '50%',
+  height: '80px',
+  alignItems: 'center',
+  display: 'flex',
+  flexDirection: 'row',
+  fontSize: '25px'
+}
+
+const headerTab: CSS.Properties = {
+  padding: '10px 0px 10px 10px',
+  color: '#FFF'
+}
+
+const styles = {
+  header,
+  headerTab
+}
+
+export default styles
