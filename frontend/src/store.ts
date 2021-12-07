2411289c2a3b30f9103092e7aa1a889d6a4fd1b9
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-12-07 09:13:53 +0200

    Add Redux DevTools

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,4 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import boardReducer from './reducers/boardReducer'
 import userReducer from './reducers/userReducer'
@@ -10,7 +11,12 @@ const reducer = combineReducers({
   user: loginReducer
 })
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const store = createStore(
+  reducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
