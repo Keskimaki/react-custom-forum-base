f5ed0d641f06d0ce5089633a10689425e53c2427
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-12-11 12:14:41 +0200

    Reconfigure replying

--- frontend/src/components/boards/posts/index.tsx
+++ frontend/src/components/boards/posts/index.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { PostType } from '../../../types'
 import styles from '../../../styles'
 import { BoardType, LoggedUser } from '../../../types'
 import { RootState } from '../../../store'
 import MakePost from './MakePost'
 import postService from '../../../services/posts'
+import { initializeBoards } from '../../../reducers/boardReducer'
 
 type Type = React.Dispatch<React.SetStateAction<string[]>>
 
@@ -40,11 +41,15 @@ const Posts = () => {
 }
 
 const Post = ({ post, responseTo, setResponseTo }: { post: PostType, responseTo: string[], setResponseTo: Type }) => {
+  const dispatch = useDispatch()
   const user: LoggedUser = useSelector((state: RootState) => state.user)
+
   const handlePostDeletion = () => {
     postService.deletePost(post.id, user.id, user.token)
-    window.location.reload() //Don't leave this here permamently
+    dispatch(initializeBoards())
+    setResponseTo([])
   }
+
   const addToReplies = () => {
     const updatedResponseTo = responseTo.some(res => res === post.id)
       ? responseTo.filter(res => res !== post.id)
@@ -53,10 +58,13 @@ const Post = ({ post, responseTo, setResponseTo }: { post: PostType, responseTo:
   }
 
   return (
-    <div style={styles.board} onClick={addToReplies}>
+    <div style={styles.board}>
       {post.responseTo} <br />
       {post.content} <br />
       {post.repliesTo}
+      <button onClick={addToReplies}>
+        reply
+      </button>
       {user.id === post.user && 
         <button onClick={handlePostDeletion}>
           delete
