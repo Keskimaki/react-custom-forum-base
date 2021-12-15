01ba1a1dd0c91acb3cdb34376b452e06c8bcb206
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-12-15 11:11:04 +0200

    Add notification if username already taken

--- frontend/src/components/CreateAccount.tsx
+++ frontend/src/components/CreateAccount.tsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
 import styles from '../styles'
 import userService from '../services/users'
 import { saveLoginData } from '../reducers/loginReducer'
 import { setNotification } from '../reducers/notificationReducer'
+import { initializeUsers } from '../reducers/userReducer'
 import loginService from '../services/login'
+import { RootState } from '../store'
 
 const CreateAccount = () => {
   const [ username, setUsername ] = useState('')
   const [ email, setEmail ] = useState('')
   const [ password, setPassword ] = useState('')
   const [ repeatPassword, setRepeatPassword ] = useState('')
+
+  const users = useSelector((state: RootState) => state.users)
+  const usernames = users.map(user => user.username)
   
   const dispatch = useDispatch()
   const navigate = useNavigate()
@@ -20,6 +25,8 @@ const CreateAccount = () => {
     event.preventDefault()
     if (username.length < 3) {
       dispatch(setNotification('Username must be at least three characters long', 'negative'))
+    } else if (usernames.some(name => name === username)) {
+      dispatch(setNotification('Username already taken', 'negative'))
     } else if (password.length < 5) {
       dispatch(setNotification('Password must be at least five characters long', 'negative'))
     } else if (password !== repeatPassword) {
@@ -29,7 +36,9 @@ const CreateAccount = () => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const loginData: any = await loginService.login(username, password) //Will alway be LoggedUser and never undefined since the account is just created
       dispatch(saveLoginData(loginData))
+      dispatch(initializeUsers())
       navigate('/boards')
+      dispatch(setNotification('Account created', 'positive'))
     }
   }
 
