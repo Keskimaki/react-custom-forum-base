56673ab83e3bae2838c02e4c38766f9c94272be2
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-12-28 10:20:05 +0200

    Test thread creation

--- frontend/cypress/integration/spec.ts
+++ frontend/cypress/integration/spec.ts
@@ -24,4 +24,28 @@ describe('Forum', () => {
     cy.contains('Logged in')
     cy.contains('Tester')
   })
+
+  describe('When logged in', () => {
+    beforeEach(() => {
+      cy.contains('Login').click()
+      cy.get('input:first').type('Tester')
+      cy.get('input:last').type('password')
+      cy.contains('login').click()
+    })
+
+    it('create thread form exists', () => {
+      cy.contains('TEST - testing').click()
+      cy.contains('Make a new thread')
+      cy.get('textarea').should('exist')
+      cy.contains('Create Thread')
+    })
+
+    it('a new thread can be created', () => {
+      cy.contains('TEST - testing').click()
+      cy.get('input:last').type(`Cypress Test`)
+      cy.get('textarea').type('Testing Thread Creation')
+      //cy.contains('Create Thread').click() //TODO testing mode for database
+      cy.contains('Cypress Test').contains('created by Tester').contains('posts: 1')
+    })
+  })
 })
