21af8c8cc0748a7b6aad9faa0029abf0ecdfe236
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-12-28 10:01:02 +0200

    Test login

--- frontend/cypress/integration/spec.ts
+++ frontend/cypress/integration/spec.ts
@@ -1,9 +1,27 @@
 describe('Forum', () => {
-  it('can be opened', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000')
+  })
+
+  it('front page can be opened', () => {
     cy.contains('TEST')
     cy.contains('board for testing purposes')
     cy.contains('Create Account')
     cy.contains('Latest Posts')
+  })
+
+  it('login form can be opened', () => {
+    cy.contains('Login').click()
+  })
+
+  it('user can log in', () => {
+    cy.contains('Login').click()
+
+    cy.get('input:first').type('Tester')
+    cy.get('input:last').type('password')
+    cy.contains('login').click()
+    
+    cy.contains('Logged in')
+    cy.contains('Tester')
   })
 })
