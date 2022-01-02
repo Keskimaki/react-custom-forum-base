41400c132102f05e5ea31302829be87582805a6a
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-01-02 12:56:46 +0200

    Serve frontend files from backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import path from 'path'
 import env from './utils/config'
 
 import userRouter from './routes/users'
@@ -15,7 +16,7 @@ void mongoose.connect(env.MONGODB_URI)
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
+app.use(express.static(path.join(__dirname, 'build')))
 
 //Allow reseting testing database
 if (process.env.NODE_ENV === 'test') {
@@ -27,5 +28,10 @@ app.use('/api/login', loginRouter)
 app.use('/api/boards', boardRouter)
 app.use('/api/threads', threadRouter)
 app.use('/api/posts', postRouter)
+
+//Serve files for frontend
+app.get('/*', function (_req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
 
 export default app
